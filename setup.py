2653cff8fd82cf7444546b688698acad8444b734
Philip J Freeman <elektron@halo.nu>
2017-02-01 21:02:51 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "pysk",
-    version = "0.0.2",
+    version = "0.1.0",
     packages = find_packages(),
     install_requires = [
         "signalk_client>=0.2,<0.3",
